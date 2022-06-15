fc77233d62962403c3f5c07150302fcc6a2a8345
Nadav Ivgi <nadav@shesek.info>
2022-06-15 14:51:13 +0300

    Load the Bitcoin Core wallet by default once its created

--- bitcoin-setup.sh
+++ bitcoin-setup.sh
@@ -2,7 +2,9 @@
 set -eo pipefail
 
 echo '🟢 setting up default bitcoin core wallet'
-bitcoin-cli loadwallet default 2> /dev/null || bitcoin-cli createwallet default &&
+bitcoin-cli getwalletinfo 2> /dev/null || bitcoin-cli createwallet default
+# Load the wallet by default on the next run (following a workspace restart)
+sed -i 's/^#wallet=/wallet=/' /workspace/bitpod/bitcoin.conf
 
 if [ "$(bitcoin-cli getblockcount)" = 0 ]; then
   echo '🟢 mining some regtest blocks'
