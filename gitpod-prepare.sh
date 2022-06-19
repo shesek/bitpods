a0bce6724aee930c02ab94cd0337c2ee063116d2
Nadav Ivgi <nadav@shesek.info>
2022-06-19 16:30:27 +0300

    Use git submodule branch tracking
    This allows using `git submodule update --remote` to fetch the latest
    commit from the remote and point the submodule to it.

--- gitpod-prepare.sh
+++ gitpod-prepare.sh
@@ -6,21 +6,24 @@ set -eox pipefail
 
 # Options: WITH_GUI, BITCOIN_REPO_{URL,BRANCH} BTCDEB_REPO_{URL,BRANCH}
 
+: ${BITCOIN_REPO_URL:?missing BITCOIN_REPO_URL}
+: ${BITCOIN_REPO_BRANCH:?missing BITCOIN_REPO_BRANCH}
+
+: ${BTCDEB_REPO_URL:=https://github.com/bitcoin-core/btcdeb}
+: ${BTCDEB_REPO_BRANCH:=master}
+
 # Modify the .gitpod.yml base docker image to the gui variant and use 'bitcoin-qt' as the bitcoind command
 if [ -n "$WITH_GUI" ]; then
   sed -i 's!shesek/bitpod:latest!shesek/bitpod:gui!' .gitpod.yml
   sed -i -r 's!^( *)bitcoind$!\1bitcoin-qt!' .gitpod.yml
 fi
 
-# Point the bitcoin git submodule to BITCOIN_REPO_URL#BITCOIN_REPO_BRANCH
-[ -z "$BITCOIN_REPO_URL" ] || git submodule set-url -- bitcoin "$BITCOIN_REPO_URL"
-: ${BITCOIN_REPO_BRANCH:=master}
-(cd bitcoin && \
-  git fetch origin "$BITCOIN_REPO_BRANCH" &&
-  git reset --hard "origin/$BITCOIN_REPO_BRANCH")
+# Update the bitcoin and btcpdeb submodules
 
-[ -z "$BTCDEB_REPO_URL" ] || git submodule set-url -- btcdeb "$BTCDEB_REPO_URL"
-: ${BTCDEB_REPO_BRANCH:=master}
-(cd btcdeb && \
-  git fetch origin "$BTCDEB_REPO_BRANCH" &&
-  git reset --hard "origin/$BTCDEB_REPO_BRANCH")
+git submodule set-url bitcoin "$BITCOIN_REPO_URL"
+git submodule set-branch --branch "$BITCOIN_REPO_BRANCH" bitcoin
+git submodule update --init --remote --depth 1 bitcoin
+
+git submodule set-url btcdeb "$BTCDEB_REPO_URL"
+git submodule set-branch --branch "$BTCDEB_REPO_BRANCH" btcdeb
+git submodule update --init --remote --depth 1 btcdeb
